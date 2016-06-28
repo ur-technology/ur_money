038a30be9394b393291964c03c05f5d618c19c8c
John Reitano <jreitano@gmail.com>
2016-06-28 14:01:30 -0700

    improve prelaunch app

--- app/prelaunch_pages/sign-in/sign-in.ts
+++ app/prelaunch_pages/sign-in/sign-in.ts
@@ -2,6 +2,7 @@ import {Page, NavController, NavParams} from 'ionic-angular';
 import {Component} from '@angular/core';
 import {FORM_DIRECTIVES, FormBuilder, ControlGroup, Validators, AbstractControl, Control} from '@angular/common';
 import {Auth} from '../../components/auth/auth';
+import {Focuser} from '../../components/focuser/focuser';
 import {FirebaseService} from '../../prelaunch_components/firebase-service/firebase-service';
 import {CustomValidators} from '../../components/custom-validators/custom-validators';
 import * as _ from 'underscore'
@@ -10,6 +11,7 @@ import {SignUpPage} from '../sign-up/sign-up';
 import {ErrorPage} from '../error/error';
 
 @Page({
+  directives: [Focuser],
   templateUrl: 'build/prelaunch_pages/sign-in/sign-in.html'
 })
 export class SignInPage {
