54ff05b74d46737e3bd0637471c20414036f1ccf
John Reitano <jreitano@gmail.com>
2016-07-01 14:52:10 -0700

    tying to fix typings install

--- typings/index.d.ts
+++
@@ -1,2 +0,0 @@
-/// <reference path="globals/google-libphonenumber/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
--- typings/main.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="main/ambient/es6-shim/index.d.ts" />
-/// <reference path="main/ambient/firebase/index.d.ts" />
-/// <reference path="main/ambient/underscore/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
-/// <reference path="globals/google-libphonenumber/index.d.ts" />
-
