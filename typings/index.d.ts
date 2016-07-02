4c92e97a7ff676e3b2dbef4e8b3865fe675bf104
nikos <nikos.efthias@gmail.com>
2016-07-02 06:36:35 +0300

    static typings folder \!dont mess with

---
+++ typings/index.d.ts
@@ -0,0 +1,2 @@
+/// <reference path="globals/google-libphonenumber/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
---
+++ typings/main.d.ts
@@ -0,0 +1,6 @@
+/// <reference path="main/ambient/es6-shim/index.d.ts" />
+/// <reference path="main/ambient/firebase/index.d.ts" />
+/// <reference path="main/ambient/underscore/index.d.ts" />
+/// <reference path="modules/lodash/index.d.ts" />
+/// <reference path="globals/google-libphonenumber/index.d.ts" />
+
