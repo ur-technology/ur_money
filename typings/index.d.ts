4b2fbb7d174faad931a97c7e73df160c1eb13047
John Reitano <jreitano@gmail.com>
2016-07-06 13:36:55 -0700

    try to get packages and typings right

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,5 @@
+/// <reference path="globals/firebase/index.d.ts" />
 /// <reference path="globals/google-libphonenumber/index.d.ts" />
+/// <reference path="main/ambient/es6-shim/index.d.ts" />
+/// <reference path="main/ambient/underscore/index.d.ts" />
 /// <reference path="modules/lodash/index.d.ts" />
