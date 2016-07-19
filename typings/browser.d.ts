4356974ee8e2e726d16333e1080fa914852890f5
John Reitano <jreitano@gmail.com>
2016-07-19 14:07:56 -0700

    remove typings/ from git; you will now need to do 'typings install' when installing dev environment

--- typings/browser.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="browser/ambient/es6-shim/index.d.ts" />
-/// <reference path="browser/ambient/firebase/index.d.ts" />
-/// <reference path="browser/ambient/underscore/index.d.ts" />
