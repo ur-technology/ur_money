a85b53653deacb8b7a8c535ecfbdd5fb5ab34309
John Reitano <jreitano@gmail.com>
2016-07-19 19:20:28 -0700

    remove extra file

--- typings/index.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference path="globals/es6-shim/index.d.ts" />
-/// <reference path="globals/firebase-timestamp/index.d.ts" />
-/// <reference path="globals/firebase/index.d.ts" />
-/// <reference path="globals/google-libphonenumber/index.d.ts" />
-/// <reference path="globals/require/index.d.ts" />
-/// <reference path="modules/lodash/index.d.ts" />
