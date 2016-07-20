11fef913007834f87917c6b0d20373961ad1817e
John Reitano <jreitano@gmail.com>
2016-07-19 19:23:49 -0700

    comment out broken script code

--- script/migrate.ts
+++ script/migrate.ts
@@ -1,18 +1,18 @@
-// migrate data
- if (this.migrate) {
-   var usersRef = this.auth.firebaseRef().child("users");
-   usersRef.once("value", function(snapshot) {
-     var allUsers = snapshot.val();
-     _.each(allUsers, function(user: any, uid: string) {
-
-       // replace the following 5 lines to do your data migration
-       var condition = false;
-       if (condition) {
-         var newData: any = {};
-         usersRef.child(uid).update(newData);
-       };
-
-     });
-   });
-
- }
+// // migrate data
+//  if (this.migrate) {
+//    var usersRef = this.auth.firebaseRef().child("users");
+//    usersRef.once("value", function(snapshot) {
+//      var allUsers = snapshot.val();
+//      _.each(allUsers, function(user: any, uid: string) {
+//
+//        // replace the following 5 lines to do your data migration
+//        var condition = false;
+//        if (condition) {
+//          var newData: any = {};
+//          usersRef.child(uid).update(newData);
+//        };
+//
+//      });
+//    });
+//
+//  }
