1cc81c2d9adb01cde18828593f99f5fe3e66ac13
Alex Dulub <alex.dulub@designsters.com>
2016-11-18 21:53:45 +0300

    Tests structure

---
+++ test/initDB.ts
@@ -0,0 +1,14 @@
+import {FIREBASE_PROVIDERS, defaultFirebase, firebaseAuthConfig, AuthProviders, AuthMethods, AngularFire} from 'angularfire2';
+import {Config} from '../app/config/config';
+
+export var defaultDB = defaultFirebase({
+    apiKey: Config.firebaseApiKey,
+    authDomain: `${Config.firebaseProjectId}.firebaseapp.com`,
+    databaseURL: `https://${Config.firebaseProjectId}.firebaseio.com`,
+    storageBucket: `${Config.firebaseProjectId}.appspot.com`
+});
+export var configDB = firebaseAuthConfig({
+    provider: AuthProviders.Custom, method: AuthMethods.CustomToken, remember: 'default' // scope: ['email']
+});
+
+export var af = new AngularFire(`https://${Config.firebaseProjectId}.firebaseio.com`, `${Config.firebaseProjectId}.firebaseapp.com`, `${Config.firebaseProjectId}.appspot.com`);
---
+++ test/mocks/Config.ts
@@ -0,0 +1,14 @@
+export class ConfigMock {
+
+    public get(): any {
+        return '';
+    }
+
+    public getBoolean(): boolean {
+        return true;
+    }
+
+    public getNumber(): number {
+        return 1;
+    }
+}
