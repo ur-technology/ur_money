2d41529559dc0e5a94dbbd329b66b86d9952a896
Viktor Pulyak <viktor@designsters.com>
2016-11-18 16:08:58 +0300

    Add auth check test

--- test/initDB.ts
+++ test/initDB.ts
@@ -10,3 +10,5 @@ export var defaultDB = defaultFirebase({
 export var configDB = firebaseAuthConfig({
     provider: AuthProviders.Custom, method: AuthMethods.CustomToken, remember: 'default' // scope: ['email']
 });
+
+export var af = new AngularFire(`https://${Config.firebaseProjectId}.firebaseio.com`, `${Config.firebaseProjectId}.firebaseapp.com`, `${Config.firebaseProjectId}.appspot.com`);
