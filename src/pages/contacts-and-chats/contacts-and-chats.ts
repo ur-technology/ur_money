a10e4ba1b5bc196767bf3bc2f579309eff3b0dde
Xavier Perez <xap5xap@gmail.com>
2016-12-17 10:18:28 -0500

    Add a share button to contacts and chats

--- src/pages/contacts-and-chats/contacts-and-chats.ts
+++ src/pages/contacts-and-chats/contacts-and-chats.ts
@@ -1,8 +1,10 @@
-import { NavController, NavParams, Platform} from 'ionic-angular';
+import { NavController, NavParams, Platform, AlertController} from 'ionic-angular';
 import {TranslateService} from 'ng2-translate/ng2-translate';
 import {AuthService} from '../../services/auth';
 import {Config} from '../../config/config';
 import { Component } from '@angular/core';
+import {SocialSharing, Clipboard, Toast} from 'ionic-native';
+import * as log from 'loglevel';
 
 declare var jQuery: any;
 
@@ -15,7 +17,7 @@ export class ContactsAndChatsPage {
   chatsPage: any;
   segmentSelected: string = 'contacts';
 
-  constructor(public nav: NavController, public navParams: NavParams, public platform: Platform, public translate: TranslateService, public auth: AuthService) {
+  constructor(public nav: NavController, public navParams: NavParams, public platform: Platform, public translate: TranslateService, public auth: AuthService, public alertCtrl: AlertController) {
     this.goal = navParams.get('goal');
   }
 
@@ -38,6 +40,36 @@ export class ContactsAndChatsPage {
       case 'invite':
         return this.translate.instant('contacts-and-chats.titleInvite');
     }
+  }
+
+  shareLink() {
+    let self = this;
+
+    if (!self.platform.is('cordova')) {
+      // HACK: this code is here to test invitations in ionic serve
+      self.alertCtrl.create({
+        title: 'Simulating social sharing action sheet',
+        message: 'This is where the social sharing action sheet is normally displayed!',
+        buttons: ['Ok']
+      }).present();
+      return;
+    }
+
+    let message = this.translate.instant('contacts.inviteMessage');
+
+    Clipboard.copy(message).then((data) => {
+      Toast.show(this.translate.instant('contacts.toastMessage'), 'long', 'top').subscribe((toast) => {
+        SocialSharing.shareWithOptions({
+          message: message, // not supported on some apps (Facebook, Instagram)
+          url: self.auth.referralLink(window),
+          chooserTitle: this.translate.instant('contacts.toastTitle') // Android only
+        }).then((result) => {
+          log.debug('returned from SocialSharing.shareWithOptions');
+        }, (error) => {
+          log.warn('Sharing failed with message: ' + error);
+        });
+      });
+    });
   }
 
 }
