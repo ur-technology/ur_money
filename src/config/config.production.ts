1c1b98cbfe521f3e7ceee26e519f2104ce30815b
John Reitano <jreitano@gmail.com>
2017-01-04 09:33:54 -0800

    point to transaction-relay-2 in production since transaction-relay-1 is too slow

--- src/config/config.production.ts
+++ src/config/config.production.ts
@@ -1,6 +1,6 @@
 export let Config: any = {
   'logLevel': 'debug',
-  'transactionRelay': 'https://relay.ur.technology:9596',
+  'transactionRelay': 'https://www.relay.ur.technology:9596',
   'firebaseProjectId': 'ur-money-production',
   'firebaseApiKey': 'AIzaSyCAfUyRbUHkuRQhKOSgmMTrMtR2vVgeJqo',
   'stripePublishableKey': 'pk_live_0AFFhzUArOgWBSlKm3IOXFwo',
