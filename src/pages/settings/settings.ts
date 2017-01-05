0f7f66be382efa54cecf47a335a19decf3f35824
Xavier Perez <xap5xap@gmail.com>
2017-01-05 17:40:12 -0500

    Allow edit firstname, lastname, middleName. Allow copy to clipboard wallet address

--- src/pages/settings/settings.ts
+++ src/pages/settings/settings.ts
@@ -1,5 +1,5 @@
 import {Component } from '@angular/core';
-import {FormGroup, FormControl} from '@angular/forms';
+import {FormGroup, FormControl, Validators} from '@angular/forms';
 import { NavController, AlertController, ToastController} from 'ionic-angular';
 import * as _ from 'lodash';
 import * as log from 'loglevel';
@@ -13,7 +13,6 @@ import {TranslateService} from 'ng2-translate/ng2-translate';
 })
 export class SettingsPage {
   public mainForm: FormGroup;
-  errorMessage: string;
   countries: any[];
   profile: any;
 
@@ -24,6 +23,32 @@ export class SettingsPage {
     public toastCtrl: ToastController, public translate: TranslateService
   ) {
 
+    this.fillCountries();
+    this.loadFormGroup();
+  }
+
+  private loadFormGroup() {
+    let authUser = this.auth.currentUser;
+    let chatNotificationsEnabled: boolean = authUser.settings && authUser.settings.chatNotifications;
+    chatNotificationsEnabled = _.isUndefined(chatNotificationsEnabled) ? true : chatNotificationsEnabled;
+    let transactionNotificationsEnabled: boolean = authUser.settings && authUser.settings.transactionNotifications;
+    transactionNotificationsEnabled = _.isUndefined(transactionNotificationsEnabled) ? true : transactionNotificationsEnabled;
+
+    this.mainForm = new FormGroup({
+      firstName: new FormControl(authUser.firstName, [CustomValidator.nameValidator, Validators.required]),
+      middleName: new FormControl(authUser.middleName , [CustomValidator.optionalNameValidator]),
+      lastName: new FormControl(authUser.lastName , [CustomValidator.nameValidator, Validators.required]),
+      name: new FormControl(authUser.name, [CustomValidator.nameValidator, Validators.required]),
+      chatNotifications: new FormControl(chatNotificationsEnabled),
+      transactionNotifications: new FormControl(transactionNotificationsEnabled),
+      countryCode: new FormControl('', Validators.required),
+    });
+
+    let country = this.countries.find((x) => { return x.alpha2 === (authUser.countryCode || 'US'); });
+    (<FormControl>this.mainForm.controls['countryCode']).setValue(country);
+  }
+
+  private fillCountries() {
     this.countries = require('country-data').countries.all.sort((a, b) => {
       return (a.name < b.name) ? -1 : ((a.name === b.name) ? 0 : 1);
     });
@@ -31,30 +56,12 @@ export class SettingsPage {
     this.countries = _.filter(this.countries, (country) => {
       return ['CU', 'IR', 'KP', 'SD', 'SY'].indexOf(country.alpha2) === -1;
     });
-
-    let authUser = this.auth.currentUser;
-    let chatNotificationsVal: boolean = authUser.settings && authUser.settings.chatNotifications;
-    chatNotificationsVal = _.isUndefined(chatNotificationsVal) ? true : chatNotificationsVal;
-    let transactionNotificationsVal: boolean = authUser.settings && authUser.settings.transactionNotifications;
-    transactionNotificationsVal = _.isUndefined(transactionNotificationsVal) ? true : transactionNotificationsVal;
-
-    this.mainForm = new FormGroup({
-      firstName: new FormControl({ value: '', disabled: true }, CustomValidator.nameValidator),
-      middleName: new FormControl({ value: '', disabled: true }, CustomValidator.optionalNameValidator),
-      lastName: new FormControl({ value: '', disabled: true }, CustomValidator.nameValidator),
-      wallet: new FormControl(authUser.wallet.address || ''),
-      chatNotifications: new FormControl(chatNotificationsVal),
-      transactionNotifications: new FormControl(transactionNotificationsVal)
-    });
-
-    this.profile = {
-      firstName: authUser.firstName || '',
-      middleName: authUser.middleName || '',
-      lastName: authUser.lastName || '',
-      country: this.countries.find((x) => { return x.alpha2 === (authUser.countryCode || 'US'); })
-    };
   }
 
+  ionViewDidEnter() {
+    let Clipboard = require('clipboard');
+    new Clipboard('#copy-button');
+  }
 
   submit() {
     this.saveProfile();
@@ -78,13 +85,14 @@ export class SettingsPage {
   saveProfile() {
     let self = this;
     let profile = {
-      firstName: self.profile.firstName,
-      middleName: self.profile.middleName,
-      lastName: self.profile.lastName,
-      countryCode: self.profile.countryCode,
+      firstName: self.mainForm.value.firstName,
+      lastName: self.mainForm.value.lastName,
+      middleName: self.mainForm.value.middleName,
+      name: self.mainForm.value.name,
+      countryCode: self.mainForm.value.countryCode.alpha2,
       settings: {
-        chatNotifications: this.mainForm.value.chatNotifications,
-        transactionNotifications: this.mainForm.value.transactionNotifications
+        chatNotifications: self.mainForm.value.chatNotifications,
+        transactionNotifications: self.mainForm.value.transactionNotifications
       }
     };
     self.auth.currentUserRef.update(_.omitBy(profile, _.isNil)).then(() => {
