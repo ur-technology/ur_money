14c1660223d9498d76b49a4798b47a9bf88911fc
Haohong Xu <weidai1122@gmail.com>
2017-03-07 00:50:25 +0800

    Update lost-password page

--- src/pages/registration/lost-password/lost-password.ts
+++ src/pages/registration/lost-password/lost-password.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CustomValidator } from '../../../validators/custom';
-import { WelcomePage } from '../welcome/welcome';
+import { ResetPasswordWithCodePage } from '../reset-password-with-code/reset-password-with-code';
 import { TranslateService } from 'ng2-translate/ng2-translate';
 import { ToastService } from '../../../services/toast';
 import { AuthService } from '../../../services/auth';
@@ -20,28 +20,37 @@ export class LostPasswordPage {
 
   submit() {
     let self = this;
-    let taskState: string;
     let loadingModal = self.loadingController.create({ content: self.translate.instant('pleaseWait') });
 
-    loadingModal.present().then(() => {
-      return self.auth.sendRecoveryEmail(self.mainForm.value.email);
-    }).then((newTaskState: string) => {
-      taskState = newTaskState;
-      return loadingModal.dismiss();
-    }).then(() => {
-      switch (taskState) {
-        case 'email-sent':
-        self.toastService.showMessage({messageKey:'sign-in.emailSent'});
-        self.navCtrl.setRoot(WelcomePage);
-          break;
-        default:
-          self.toastService.showMessage({ messageKey: 'sign-in.unexpectedProblem' });
-
-      }
+    loadingModal.present()
+      .then(() => {
+        return self.auth.sendResetCode(self.mainForm.value.email);
+      })
+      .then((taskState: string) => {
+        loadingModal
+          .dismiss()
+          .then(() => {
+            switch (taskState) {
+              case 'send_reset_code_finished':
+                self.toastService.showMessage({ messageKey: 'sign-in.sentResetCode' });
+                self.navCtrl.setRoot(ResetPasswordWithCodePage);
+                break;
+              case 'send_reset_code_canceled_because_user_not_found':
+                self.toastService.showMessage({ messageKey: 'sign-in.emailNotFound'});
+                break;
+              case 'send_reset_code_canceled_because_user_disabled':
+                self.toastService.showMessage({ messageKey: 'sign-in.userDisabled'});
+                break;
+              default:
+                self.toastService.showMessage({ messageKey: 'sign-in.unexpectedProblem' });
+            }
+          });
     }, (error) => {
-      loadingModal.dismiss().then(() => {
-        self.toastService.showMessage({ messageKey: 'sign-in.unexpectedProblem' });
-      });
+      loadingModal
+        .dismiss()
+          .then(() => {
+            self.toastService.showMessage({ messageKey: 'sign-in.unexpectedProblem' });
+        });
     });
   }
 
