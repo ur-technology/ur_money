1917d4bc6ed2bcd2cacb571a29fc282a78ffae14
Haohong Xu <weidai1122@gmail.com>
2017-03-10 22:10:58 +0800

    Update reset-password-with-code page component

--- src/pages/registration/reset-password-with-code/reset-password-with-code.ts
+++ src/pages/registration/reset-password-with-code/reset-password-with-code.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController, NavParams, LoadingController } from 'ionic-angular';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CustomValidator } from '../../../validators/custom';
 import { TranslateService } from 'ng2-translate/ng2-translate';
@@ -12,22 +12,19 @@ import { SignInPage } from '../sign-in/sign-in';
   templateUrl: 'reset-password-with-code.html'
 })
 export class ResetPasswordWithCodePage {
+  private resetCode: string;
   mainForm: FormGroup;
-  phone: string;
 
   constructor(
     public navCtrl: NavController,
+    private navParams: NavParams,
     private auth: AuthService,
     private toastService: ToastService,
     private translate: TranslateService,
     private loadingController: LoadingController
   ) {
+    this.resetCode = this.navParams.get('resetCode');
     this.mainForm = new FormGroup({
-      resetCode: new FormControl('', [
-        Validators.required,
-        Validators.minLength(6),
-        Validators.maxLength(6)
-      ]),
       password: new FormControl('', [
         Validators.required,
         Validators.minLength(6),
@@ -42,44 +39,45 @@ export class ResetPasswordWithCodePage {
   }
 
   submit() {
-    let self = this;
-    let loadingModal = self.loadingController.create({ content: self.translate.instant('pleaseWait') });
+    const loadingModal = this.loadingController
+      .create({
+        content: this.translate.instant('pleaseWait')
+      });
     
     loadingModal
       .present()
-      .then(() => {
-        return this.auth.generateHashedPassword(self.mainForm.value.password);
-      })
-      .then((hashedPassword) => {
-        return this.auth.resetPasswordWithCode(
-          self.mainForm.value.resetCode,
-          hashedPassword,
-        );
-      })
+      .then(() => this.auth.generateHashedPassword(this.mainForm.value.password))
+      .then((hashedPassword) => this.auth.resetPasswordWithCode(
+        this.resetCode,
+        hashedPassword,
+      ))
       .then((taskState: string) => {
         loadingModal
           .dismiss()
           .then(() => {
             switch (taskState) {
               case 'reset_password_finished':
-                self.toastService.showMessage({ messageKey: 'sign-in.resetPassword' });
-                self.navCtrl.setRoot(SignInPage);
+                this.toastService.showMessage({ messageKey: 'sign-in.resetPassword' });
+                this.navCtrl.setRoot(SignInPage);
                 break;
+
               case 'reset_password_canceled_because_user_not_found':
-                self.toastService.showMessage({ messageKey: 'sign-in.resetCodeNotFound'});
+                this.toastService.showMessage({ messageKey: 'sign-in.resetCodeNotFound'});
                 break;
+
               case 'reset_password_canceled_because_user_disabled':
-                self.toastService.showMessage({ messageKey: 'sign-in.userDisabled'});
+                this.toastService.showMessage({ messageKey: 'sign-in.userDisabled'});
                 break;
+
               default:
-                self.toastService.showMessage({ messageKey: 'sign-in.unexpectedProblem' });
+                this.toastService.showMessage({ messageKey: 'sign-in.unexpectedProblem' });
             }
-          })
+          });
       }, (error) => {
         loadingModal
           .dismiss()
           .then(() => {
-            self.toastService.showMessage({ messageKey: 'sign-in.unexpectedProblem' });
+            this.toastService.showMessage({ messageKey: 'sign-in.unexpectedProblem' });
           });
       });
   }
