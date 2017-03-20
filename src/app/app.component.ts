63ed67e3dedafb0f6dea3a26a566419d98523ccb
Xavier Perez <xap5xap@gmail.com>
2017-03-20 16:03:43 -0500

    Add invite friends menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -169,9 +169,8 @@ export class UrMoney {
       }
 
       this.menuItems.push({ title: 'Send UR', page: SendPage, pageParams: { contact: {} }, icon: 'icon menu-icon menu-icon-send-ur', value: 'send' });
-
-      // this.menuItems.push({ title: 'Request UR', page: ContactsAndChatsPage, pageParams: { goal: 'request' }, icon: 'icon menu-icon menu-icon-request-ur', value: 'request' });
       this.menuItems.push({ title: 'Transactions', page: TransactionsPage, icon: 'icon menu-icon menu-icon-transactions', value: 'transactions' });
+      this.menuItems.push({ title: 'Invite friends', page: Config.targetPlatform === 'web' ? InviteLinkPage : ContactsAndChatsPage, pageParams: { goal: 'invite' }, icon: 'icon menu-icon menu-icon-invite', value: 'inviteFriends' });
     }
     this.menuItems.push({ title: 'About UR', page: AboutPage, icon: 'icon menu-icon menu-icon-about', value: 'about' });
     if (this.auth.currentUser && this.auth.currentUser.admin) {
