14ded488054ebef9f66c98eeced91e48df1188a0
Haohong Xu <weidai1122@gmail.com>
2017-03-28 04:14:31 +0800

    Trigger the processor that sends out a verification email at user sign-up

--- src/pages/registration/profile-setup/profile-setup.ts
+++ src/pages/registration/profile-setup/profile-setup.ts
@@ -1,13 +1,16 @@
-import { NavController, AlertController } from 'ionic-angular';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { TranslateService } from 'ng2-translate/ng2-translate';
 import * as _ from 'lodash';
 import * as log from 'loglevel';
 import * as firebase from 'firebase';
+
+import { NavController, AlertController, LoadingController } from 'ionic-angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { TranslateService } from 'ng2-translate/ng2-translate';
+import { Component } from '@angular/core';
+
 import { AuthService } from '../../../services/auth';
+import { ToastService } from '../../../services/toast';
 import { CustomValidator } from '../../../validators/custom';
 import { WalletSetupPage } from '../../../pages/registration/wallet-setup/wallet-setup';
-import { Component } from '@angular/core';
 
 @Component({
   selector: 'profile-setup-page',
@@ -19,8 +22,10 @@ export class ProfileSetupPage {
   constructor(
     public nav: NavController,
     public auth: AuthService,
+    public toast: ToastService,
     private translate: TranslateService,
-    public alertCtrl: AlertController
+    private alertCtrl: AlertController,
+    private loadingController: LoadingController
   ) {
     let formElements: any = {
       name: new FormControl('', [CustomValidator.nameValidator, Validators.required]),
@@ -53,15 +58,57 @@ export class ProfileSetupPage {
   }
 
   submit() {
-    this.auth.currentUser.update(this.mainForm.value).then(() => {
+    let loadingModal = this.loadingController.create({ content: this.translate.instant('pleaseWait') });
 
-      firebase.database().ref('/manualIDVerification/tasks').push({
-        id: this.auth.currentUserId
-      }).then();
+    loadingModal
+      .present()
+      .then(() => {
+        return this.auth.currentUser.update(this.mainForm.value);
+      })
+      .then(() => {
+        return firebase
+          .database()
+          .ref('/manualIDVerification/tasks')
+          .push({
+            id: this.auth.currentUserId
+          });
+      })
+      .then(() => {
+        return this.auth.sendVerificationEmail(
+          this.auth.currentUser.phone,
+          this.auth.currentUser.email
+        );
+      })
+      .then((taskState: string) => {
+        loadingModal
+          .dismiss()
+          .then(() => {
+            switch (taskState) {
+              case 'send_verification_email_finished':
+                this.toast.showMessage({ messageKey: 'verify-email.verifyEmailSent' });
+                break;
 
-      this.nav.push(WalletSetupPage);
-    }).catch((error) => {
-      log.warn('unable to save profile info');
-    });
-  };
+              case 'send_verification_email_canceled_because_user_not_found':
+                this.toast.showMessage({ messageKey: 'errors.emailNotFound'});
+                break;
+
+              case 'send_verification_email_canceled_because_user_disabled':
+                this.toast.showMessage({ messageKey: 'errors.userDisabled'});
+                break;
+
+              default:
+                this.toast.showMessage({ messageKey: 'errors.unexpectedProblem' });
+            }
+          });
+
+        this.nav.push(WalletSetupPage);
+      }, (error) => {
+        loadingModal
+          .dismiss()
+          .then(() => {
+            this.toast.showMessage({ messageKey: 'errors.unexpectedProblem' });
+            log.warn('unable to save profile info');
+          });
+      });
+    };
 }
