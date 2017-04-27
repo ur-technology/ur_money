73c7c5c3573c015be5c090730c7e1690dbd32e83
Haohong Xu <weidai1122@gmail.com>
2017-04-27 22:51:16 +0800

    Add terms and conditions page object

---
+++ features/pages/terms-and-conditions.page.ts
@@ -0,0 +1,31 @@
+import { $, browser, ExpectedConditions } from 'protractor';
+
+export class TermsAndConditionsPageObject {
+  public termsMessage: any;
+  public termsLink: any;
+  public modal: any;
+  public modalHeader: any;
+  public modalTitle: any;
+  public modalDoneButton: any;
+
+  constructor() {
+    this.termsMessage = $('p.terms');
+    this.termsLink = this.termsMessage.$('a.terms-btn');
+    this.modal = $('ion-modal');
+    this.modalHeader = this.modal.$('ion-header');
+    this.modalTitle = this.modalHeader.$('ion-title');
+    this.modalDoneButton = this.modalHeader.$('button.bar-button');
+  }
+
+  openTermsAndConditions() {
+    return browser.wait(ExpectedConditions.elementToBeClickable(this.termsLink), 3000)
+    .then(() => this.termsLink.click())
+    .then(() => browser.sleep(3000));
+  }
+
+  exitTermsAndConditions() {
+    return browser.wait(ExpectedConditions.elementToBeClickable(this.modalDoneButton), 3000)
+    .then(() => this.modalDoneButton.click())
+    .then(() => browser.sleep(3000));
+  }
+}
