5ca548ead424d28a65819dcebb52bc829c3df245
Xavier Perez <xap5xap@gmail.com>
2017-05-15 17:04:26 -0500

    Add signIn page unit tests

--- src/mocks.ts
+++ src/mocks.ts
@@ -12,3 +12,30 @@ export class GoogleAnalyticsEventsServiceMock {
 
   public emitCurrentPage(pageName: string): void { }
 }
+
+export class CountryListServiceMock {
+  public getCountryData(): void { }
+  public getDefaultContry(): any { }
+}
+
+export class AuthServiceMock {
+
+}
+
+export class ModalControllerMock {
+  public create(): any {
+    return { present: () => { } };
+  }
+}
+
+export class LoadingControllerMock {
+
+}
+
+export class ToastServiceMock {
+
+}
+
+export class AlertControllerMock {
+
+}
