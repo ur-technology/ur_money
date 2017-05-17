7dcd7290d0887299e26e4e563f1621adf7c4fb2b
Xavier Perez <xap5xap@gmail.com>
2017-05-17 14:43:15 -0500

    Add more sign in page unit tests

--- src/mocks.ts
+++ src/mocks.ts
@@ -15,7 +15,7 @@ export class GoogleAnalyticsEventsServiceMock {
 
 export class CountryListServiceMock {
   public getCountryData(): any[] {
-    return[
+    return [
       {
         'telephoneCountryCode': '+93',
         'name': 'Afghanistan',
@@ -31,7 +31,7 @@ export class CountryListServiceMock {
         'name': 'United States',
         'countryCode': 'US'
       }];
-    }
+  }
 
   public getDefaultContry(): any {
     return {
@@ -43,7 +43,11 @@ export class CountryListServiceMock {
 }
 
 export class AuthServiceMock {
-
+  requestSignIn(phone: string): Promise<any> {
+    return new Promise(function(resolve: Function): void {
+      resolve();
+    });
+  }
 }
 
 export class ModalControllerMock {
@@ -52,14 +56,38 @@ export class ModalControllerMock {
   }
 }
 
-export class LoadingControllerMock {
+export class LoadingComponentMock {
+  present() {
+    return new Promise(function(resolve: Function): void {
+      resolve();
+    });
+  }
 
+  dismiss() {
+    return new Promise(function(resolve: Function): void {
+      resolve();
+    });
+  }
+
+}
+export class LoadingControllerMock {
+  component: LoadingComponentMock = new LoadingComponentMock();
+
+  create(): LoadingComponentMock {
+    return this.component;
+  }
 }
 
 export class ToastServiceMock {
-
+  public showMessage(): any {
+    return new Promise(function(resolve: Function): void {
+      resolve();
+    });
+  }
 }
 
 export class AlertControllerMock {
-
+  public create(): any {
+    return { present: () => { } };
+  }
 }
